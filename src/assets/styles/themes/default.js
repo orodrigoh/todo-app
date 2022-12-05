da5591f135c76fd3471864f893d6b2710e713a15
Rodrigo <rodrigo.hss@outlook.com>
2022-12-05 19:40:22 -0300

    updates

---
+++ src/assets/styles/themes/default.js
@@ -0,0 +1,22 @@
+export default {
+  colors: {
+    background: '#f6f5fc',
+    primary: {
+      lighter: '#E0E3FF',
+      light: '#6674f4',
+      main: '#5061fc',
+      dark: '#3346f0',
+    },
+    gray: {
+      900: '#222222',
+      200: '#bcbcbc',
+      100: '#E6E6E6',
+
+    },
+    danger: {
+      light: '#F97171',
+      main: '#fc5850',
+      dark: '#F63131',
+    },
+  },
+};
---
+++ src/components/app/App.js
@@ -0,0 +1,15 @@
+import { ThemeProvider } from 'styled-components';
+import GlobalStyles from '../../assets/styles/global';
+import defaultTheme from '../../assets/styles/themes/default';
+import { Container } from './styles';
+
+function App() {
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <Container />
+    </ThemeProvider>
+  );
+}
+
+export default App;
